018f53dda0e0a2baae247b84d484684c7f5bc0dd
Prashant Shelar <prashant_shelar@yahoo.com>
2025-04-08 15:22:46 +0530

    Latest Update  with database

--- action/labAction.php
+++ action/labAction.php
@@ -4,7 +4,7 @@ include './dbconnect.php';
 include './dboperation.php';
 $action = $_POST['action'];
 
-
+//ADD
 if ($action == "add") {
     if (AddOperation("lab_report", $_POST, $conn) == 1) {
 
@@ -18,7 +18,20 @@ if ($action == "add") {
     echo json_encode($response);
 }
 
-//SINGLE RECORD
+//EDIT
+if ($action == "edit") {
+    if (UpdateOperation("lab_report", $_POST, $conn) == 1) {
+        $response['status'] = true;
+        $response['message'] = "Update Successfully";
+    } else {
+        $response['status'] = false;
+        $response['message'] = "Somthing Wrong Input";
+    }
+    $conn->close();
+    echo json_encode($response);
+}
+
+//GET SINGLE RECORD
 if ($action == "singlerecord") {
     $result = getsinglerecord($_POST['table_name'], $_POST['key'], $conn, $_POST['field']);
     $conn->close();
